0870f46b018fbc6ab25c3a87e659b48fc63619f5
李凡 <lf2941270@163.com>
2015-03-31 00:15:15 +0800

    still not work..

--- index.js
+++ index.js
@@ -5,25 +5,36 @@
 var express,
     ghost,
     parentApp,
-    errors;
+		errors = require('./core/server/errors');
+;
 
 // Make sure dependencies are installed and file system permissions are correct.
-require('./core/server/utils/startup-check').check();
+//require('./core/server/utils/startup-check').check();
 
 // Proceed with startup
-express = require('express');
-ghost = require('./core');
-errors = require('./core/server/errors');
+require('./start.js')().then(function(stdout){
+	console.log(stdout);
+	return Promise.resolve();
+}).then(function(){
+			// Proceed with startup
+			express = require('express');
+			ghost = require('./core');
 
 // Create our parent express app instance.
-parentApp = express();
+			parentApp = express();
 
-ghost().then(function (ghostServer) {
-    // Mount our ghost instance on our desired subdirectory path if it exists.
-    parentApp.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
+			ghost().then(function (ghostServer) {
+				// Mount our ghost instance on our desired subdirectory path if it exists.
+				parentApp.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
 
-    // Let ghost handle starting our server instance.
-    ghostServer.start(parentApp);
-}).catch(function (err) {
-    errors.logErrorAndExit(err, err.context, err.help);
-});
+				// Let ghost handle starting our server instance.
+				ghostServer.start(parentApp);
+			}).catch(function (err) {
+						errors.logErrorAndExit(err, err.context, err.help);
+					});
+
+		})
+		.catch(function(err){
+			console.error(err);
+			errors.logErrorAndExit(err, err.context, err.help);
+		})
--- start.js
+++ start.js
@@ -1,2 +1,65 @@
-//var child = require('child_process');
-//child.exec('npm install && grunt init');
+var child = require('child_process');
+var Promise = require('bluebird');
+module.exports = function(){
+	/*function bower(){
+		child.exec('bower install', function(error, stdout, stderr){
+			if(error !== null){
+				return Promise.reject(error);
+			}
+			if(stdout){
+				return Promise.resolve(stdout);
+			}
+			return Promise.reject(stderr);
+		})
+	}
+	function grunt(){
+		child.exec('grunt init', function(error, stdout, stderr){
+			if(error !== null){
+				return Promise.reject(error);
+			}
+			if(stdout){
+				return Promise.resolve(stdout);
+			}
+			return Promise.reject(stderr);
+		})
+	}*/
+	function bower(){
+		return new Promise(function(resolve, reject){
+			child.exec('bower install', function(error, stdout, stderr){
+				if(error !== null){
+					return reject(error);
+				}
+				if(stderr){
+					return reject(stderr);
+				}
+				return resolve(stdout);
+			})
+		})
+
+	}
+	function grunt(){
+		return new Promise(function(resolve, reject){
+			child.exec('grunt init', function(error, stdout, stderr){
+				if(error !== null){
+					return reject(error);
+				}
+				if(stderr){
+					return reject(stderr);
+				}
+				return resolve(stdout);
+			})
+		})
+	}
+
+	return new Promise(function(resolve, reject){
+		bower().then(function(){
+			return grunt()
+		}).then(function(stdout){
+					resolve(stdout);
+				})
+				.catch(function(err){
+					reject(err);
+				})
+	});
+
+}
