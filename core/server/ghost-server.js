61f6c1dbead61f3a3f650ae6b0eb1cc6de25a9e9
李凡 <lf2941270@163.com>
2015-03-31 12:27:12 +0800

    注释掉 self.logStartMessages(); BAE不兼容

--- core/server/ghost-server.js
+++ core/server/ghost-server.js
@@ -96,8 +96,10 @@ GhostServer.prototype.logStartMessages = function () {
     }
     // ensure that Ghost exits correctly on Ctrl+C and SIGTERM
     process.
-        removeAllListeners('SIGINT').on('SIGINT', shutdown).
-        removeAllListeners('SIGTERM').on('SIGTERM', shutdown);
+        removeAllListeners('SIGINT').
+		    on('SIGINT', shutdown).
+        removeAllListeners('SIGTERM').
+		    on('SIGTERM', shutdown);
 };
 
 GhostServer.prototype.logShutdownMessages = function () {
@@ -163,7 +165,7 @@ GhostServer.prototype.start = function (externalApp) {
         });
         self.httpServer.on('connection', self.connection.bind(self));
         self.httpServer.on('listening', function () {
-//            self.logStartMessages();
+            self.logStartMessages();
             clearTimeout(self.upgradeWarning);
             resolve(self);
         });
